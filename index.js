7ed22f10889653a6b8b2f2ca631dcd256d110522
Diegoxo <dieguitoarmandofernandez@gmail.com>
2023-10-09 19:09:14 -0500

    Deployment

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import conectarDB from "./config/db.js";
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import pacienteRoutes from "./routes/pacienteRoutes.js";
+
+const app = express();
+app.use(express.json()); // Esto es para ver lo que se le mando al servidor.
+
+dotenv.config();
+
+conectarDB();
+
+const dominiosPermitidos = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if(dominiosPermitidos.indexOf(origin) !== -1 ) {
+            //El origen del request esta permitido
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
+
+app.use('/api/veterinarios', veterinarioRoutes);
+app.use('/api/pacientes', pacienteRoutes);
+
+const PORT = process.env.PORT || 4000; // En un servidor para node JS, va a existir una variable de entorno PORT
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en el puerto ${PORT}`)
+});
